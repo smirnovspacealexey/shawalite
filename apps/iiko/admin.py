d58d5d5698269dcd4adc7c56a7fcd1170a3814f5
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2024-06-14 08:49:06 +0500

    add liko

--- apps/iiko/admin.py
+++ apps/iiko/admin.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
-from models import iikoSettings
-
-
-@admin.register(iikoSettings)
-class iikoSettingsAdmin(admin.ModelAdmin):
-    list_display = ['url', 'active']
-    list_editable = ('url', 'active', )
-
-
-
+# from django.contrib import admin
+# from models import iikoSettings
+#
+#
+# @admin.register(iikoSettings)
+# class iikoSettingsAdmin(admin.ModelAdmin):
+#     list_display = ['url', 'active']
+#     list_editable = ('url', 'active', )
+#
+#
+#
