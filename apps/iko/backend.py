cf6fa8a52e27c2cc2076c9f9b0b1028202c3ef23
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2024-06-14 09:41:33 +0500

    add liko

--- apps/iko/backend.py
+++ apps/iko/backend.py
@@ -16,21 +16,21 @@ def get_token():
 
         iikoSettings.currenttoken = data
         iikoSettings.save()
-        Log.add_new(f'get_token() {data}', 'Iiko')
+        Log.add_new(data, 'Iiko', title2='get_token()')
         return data
 
     except requests.exceptions.HTTPError as err:
         print(f'HTTP error occurred: {err}')  # Выводит ошибку HTTP
-        Log.add_new(f'get_token() ERROR: {err}', 'Iiko')
+        Log.add_new(f'HTTP error occurred: {err}', 'Iiko', title2='get_token()')
     except requests.exceptions.ConnectionError as err:
         print(f'Connection error occurred: {err}')  # Выводит ошибку соединения
-        Log.add_new(f'get_token() ERROR: {err}', 'Iiko')
+        Log.add_new(f'Connection error occurred: {err}', 'Iiko', title2='get_token()')
     except requests.exceptions.Timeout as err:
         print(f'Timeout error occurred: {err}')  # Выводит ошибку таймаута
-        Log.add_new(f'get_token() ERROR: {err}', 'Iiko')
+        Log.add_new(f'Timeout error occurred: {err}', 'Iiko', title2='get_token()')
     except requests.exceptions.RequestException as err:
         print(f'Request error occurred: {err}')  # Выводит любую другую ошибку запроса
-        Log.add_new(f'get_token() ERROR: {err}', 'Iiko')
+        Log.add_new(f'Request error occurred: {err}', 'Iiko', title2='get_token()')
 
 
 def drop_token():
@@ -40,7 +40,7 @@ def drop_token():
     response.raise_for_status()
     data = response.json()
     print(data)
-    Log.add_new(f'drop_token(): {data}', 'Iiko')
+    Log.add_new(data, 'Iiko', title2='drop_token()')
     return data
 
 
@@ -56,7 +56,7 @@ def get_kitchenorders():
 
     data = response.json()
     print(data)
-    Log.add_new(f'get_kitchenorders(): {data}', 'Iiko')
+    Log.add_new(data, 'Iiko', title2='get_kitchenorders()')
     return data
 
 
