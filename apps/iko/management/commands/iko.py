37470edbd58aca11cabdeff868eaa44484a19018
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2024-06-14 11:02:36 +0500

    add liko

--- apps/iko/management/commands/iko.py
+++ apps/iko/management/commands/iko.py
@@ -6,8 +6,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('--------iiko---------')
         get_token()
-        # get_kitchenorders()
-        # print(sber.check_order_status(2))
+        get_kitchenorders()
         print('---------------------')
 
 
