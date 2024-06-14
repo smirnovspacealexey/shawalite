3f286be5442093330fdaf08c10d17ca2c098992f
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2024-06-14 08:04:40 +0500

    add liko

--- apps/Iiko/models.py
+++ apps/Iiko/models.py
@@ -1,14 +1,14 @@
-# from django.db import models
-# from django import forms
-# from django.utils import timezone
-#
-#
-# class iikoSettings(models.Model):
-#     url = forms.CharField(widget=forms.PasswordInput())
-#     active = models.BooleanField('active', default=True)
-#
-#     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-#         if self.active:
-#             type(self).objects.exclude(pk=self.pk).update(active=False)
-#         super().save()
-#
+from django.db import models
+from django import forms
+from django.utils import timezone
+
+
+class iikoSettings(models.Model):
+    url = models.CharField(max_length=200)
+    active = models.BooleanField('active', default=True)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.active:
+            type(self).objects.exclude(pk=self.pk).update(active=False)
+        super().save()
+
