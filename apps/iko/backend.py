a68b4ebb3c6133eb07fd0c12c22407359fcd96f1
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2024-07-10 21:01:23 +0500

    add new

--- apps/iko/backend.py
+++ apps/iko/backend.py
@@ -68,7 +68,7 @@ def pull_kitchenorders():
     current_mill = round(time.time() * 1000)
     Log.add_new(str(current_mill) + ' ' + str(iko.last_getting), 'Iiko', title2='drop_token()')
 
-    if current_mill - int(iko.last_getting) > 2000:
+    if current_mill - int(iko.last_getting) > 20000:
         iko.last_getting = current_mill
         iko.save()
 
