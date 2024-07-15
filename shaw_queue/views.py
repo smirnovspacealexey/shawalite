62a3d62f6ad2a280914d137cbb8602b61fb87a9a
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2024-07-15 20:00:50 +0500

    add new

--- shaw_queue/views.py
+++ shaw_queue/views.py
@@ -1407,8 +1407,8 @@ def buyer_queue_ajax(request, template, vertical=False):
 
     template = loader.get_template('shaw_queue/buyer_queue_ajax.html')
     data = {
-        'html': template.render(context, request),
-        # 'html': 'TEST TEST',
+        # 'html': template.render(context, request),
+        'html': 'TEST TEST',
         'new_voice': new_voice,
         'kitchenorders': str(kitchenorders),
         'ready': '',
