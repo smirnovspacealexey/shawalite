d0db61b025a8288c32ca67778e2a2999439f7dcf
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2025-02-05 14:29:05 +0500

    iko

--- apps/iko/backend.py
+++ apps/iko/backend.py
@@ -85,6 +85,7 @@ def pull_kitchenorders(idiko=None):
         except Exception as e:
             Log.add_new(str(e), 'Iiko', title2='error 2')
             data = eval(iko.orders)
+            drop_token(idiko)
             Log.add_new("from base", 'Iiko', title2='orders')
 
     else:
