4b721ee8f0c7019fb50c4351fcc8a5287ace73dd
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2025-02-05 15:22:39 +0500

    iko

--- apps/logs/admin.py
+++ apps/logs/admin.py
@@ -4,7 +4,7 @@ from .models import Log
 
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'name', 'title2', 'title3', 'logg', 'datetime']
+    list_display = ['name', 'title2', 'title3', 'logg', 'datetime']
     # list_editable = ('name',)
     search_fields = ['name', 'title2', 'title3']
     list_filter = ['datetime', ]
